c8d6040b683a8f8d16f13b1782f8a5d419a5be14
Lianin1 <lian.hsiieh@gmail.com>
2024-11-21 09:28:56 +0800

    1121微調

--- Assets/Scripts/huggingFaceManager.cs
+++ Assets/Scripts/huggingFaceManager.cs
@@ -13,45 +13,20 @@ namespace Lian
     ///</summary>
     public class HuggingFaceManager : MonoBehaviour
     {
-        private string url = "https://api-inference.huggingface.co/models/sentence-transformers/all-MiniLM-L6-v2";
+        private string model = "https://api-inference.huggingface.co/models/sentence-transformers/all-MiniLM-L6-v2";
         private string key = "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";
 
-        private TMP_InputField inputField;
-        private string prompt;
-        private string role = "你是一個神祕的巫師";
+        private TMP_InputField inputFieldPlayer;
 
         private void Awake()
         {
-            inputField = GameObject.Find("InputField_llm").GetComponent<TMP_InputField>();
-            inputField.onEndEdit.AddListener(PlayerInput);
+            inputFieldPlayer = GameObject.Find("InputField_llm").GetComponent<TMP_InputField>();
+            inputFieldPlayer.onEndEdit.AddListener(PlayerInput);
         }
 
         private void PlayerInput(string input)
         {
-            print($"<color=#3f3>{input}</color>");
-            prompt = input;
-            StartCoroutine(GetResult());
+            print($"<color=#3f3>玩家輸入：{input}</color>");
         }
-
-        private IEnumerator GetResult()
-        {
-            var input = new
-            {
-                source_sentence = prompt,
-	            sentences = ""
-            };
-
-            string json = JsonUtility.ToJson(input);
-            byte[] postData = Encoding.UTF8.GetBytes(json);
-
-            UnityWebRequest request = new UnityWebRequest(url, "POST");
-            request.uploadHandler = new UploadHandlerRaw(postData);
-            request.SetRequestHeader("Content-Type", "application/json");
-            request.SetRequestHeader("Authorization", "Bearer " + key);
-
-            yield return request.SendWebRequest();
-
-            print(request.result);
-        }
-    };
+    }
 }
