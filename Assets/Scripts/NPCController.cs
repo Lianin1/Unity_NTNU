47f88d7222b77c93f5a6324956b9236700e0c7a8
Lianin1 <lian.hsiieh@gmail.com>
2024-11-28 12:23:54 +0800

    1128

--- Assets/Scripts/NPCController.cs
+++ Assets/Scripts/NPCController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Lian
@@ -10,15 +11,27 @@ namespace Lian
         // 序列化欄位：將私人變數顯示在 Unity 屬性面板
         [SerializeField, Header("NPC 資料")]
         private DataNPC dataNPC;
+        [SerializeField, Header("動畫參數")]
+        private string[] parameters =
+        {
+            "攻擊 1", "攻擊 2", "攻擊 3", "死亡"
+        };
 
         // Unity 的動畫控制系統
         private Animator ani;
+
+        public DataNPC data => dataNPC;
 
         // 喚醒事件 播放遊戲時會執行一次
         private void Awake()
         {
             // 獲得 NPC 身上的動畫控制器
             ani = GetComponent<Animator>();
+        }
+
+        public void PlayAinmation (int Index)
+        {
+            ani.SetTrigger(parameters[Index]);
         }
     }
 }
--- Assets/Scripts/huggingFaceManager.cs
+++ Assets/Scripts/huggingFaceManager.cs
@@ -1,10 +1,12 @@
-using System;
-using System.Collections;
-using System.Text;
 using TMPro;
-using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.Networking;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Lian
 {
@@ -13,20 +15,74 @@ namespace Lian
     ///</summary>
     public class HuggingFaceManager : MonoBehaviour
     {
-        private string model = "https://api-inference.huggingface.co/models/sentence-transformers/all-MiniLM-L6-v2";
+        private string url = "https://api-inference.huggingface.co/models/sentence-transformers/all-MiniLM-L6-v2";
         private string key = "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";
 
-        private TMP_InputField inputFieldPlayer;
+        private TMP_InputField inputField;
+        private string prompt;
+        private string role = "你是一位巫師";
+        private string[] npcSentences;
+
+        [SerializeField, Header("NPC 物件")]
+        private NPCController npc;
 
         private void Awake()
         {
-            inputFieldPlayer = GameObject.Find("InputField_llm").GetComponent<TMP_InputField>();
-            inputFieldPlayer.onEndEdit.AddListener(PlayerInput);
+            inputField = GameObject.Find("InputField_llm").GetComponent<TMP_InputField>();
+            inputField.onEndEdit.AddListener(PlayerInput);
+            npcSentences = npc.data.sentences;
         }
 
         private void PlayerInput(string input)
         {
             print($"<color=#3f3>玩家輸入：{input}</color>");
+            prompt = input;
+            StartCoroutine(GetResult());
+        }
+
+        private IEnumerator GetResult()
+        {
+            var inputs = new
+            {
+                source_sentence = prompt,
+                sentences = npcSentences
+            };
+
+            string json = JsonConvert.SerializeObject(inputs);
+            byte[] postData = Encoding.UTF8.GetBytes(json);
+            UnityWebRequest request = new UnityWebRequest(url, "POST");
+            request.uploadHandler = new UploadHandlerRaw(postData);
+            request.downloadHandler = new DownloadHandlerBuffer();
+            request.SetRequestHeader("Content-Type", "application/json");
+            request.SetRequestHeader("Authorization", "Bearer " + key);
+
+            yield return request.SendWebRequest();
+
+            if (request.result != UnityWebRequest.Result.Success)
+            {
+                print($"<color=#f33>要求失敗：{request.error}</color>");
+            }
+            else
+            {
+                string reponseText = request.downloadHandler.text;
+                var reponse = JsonConvert.DeserializeObject<List<float>>(reponseText);
+
+                print($"<color=#3f3>分數：{reponseText}</color>");
+
+                if (reponse != null && reponse.Count > 0)
+                {
+                    int best = reponse.Select((value, index) => new
+                    {
+                        value = value,
+                        index = index
+                    }).OrderByDescending(x => x.value).First().index;
+
+                    print($"<color=#77f>最佳結果:{npcSentences[best]}</color>");
+
+                    npc.PlayAinmation(best);
+                }
+            }
+
         }
     }
 }
